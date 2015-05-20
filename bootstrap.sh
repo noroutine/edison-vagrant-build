311c021842824632b3a4b3a864c6474acf769fa1
Oleksii Khilkevych <o.khilkevich@gastrofix.com>
2015-05-20 23:19:37 +0200

    run edison build steps on provisioning

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,24 @@
-echo "Installing..."
+#!/usr/bin/env bash
+
+
+echo "Installing necessary toolset..."
+
+sudo apt-get update
+sudo apt-get install -y build-essential git diffstat gawk chrpath texinfo libtool gcc-multilib
+
+echo "Downloading edison image sources"
+
+curl -O http://downloadmirror.intel.com/24910/eng/edison-src-ww18-15.tgz
+
+echo "Unpacking..."
+
+tar zxf edison-src-ww18-15.tgz
+
+cd edison-src
+
+
+echo "Setup..."
+make setup
+
+echo "Building..."
+make image
