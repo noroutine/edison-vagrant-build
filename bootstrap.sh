d46ef24c3c623be756b286f04150ca8b2d1314a2
Oleksii Khilkevych <o.khilkevich@gastrofix.com>
2015-05-20 23:42:50 +0200

    existing source should be wiped

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,14 +8,15 @@ sudo apt-get install -y build-essential git diffstat gawk chrpath texinfo libtoo
 
 echo "Downloading edison image sources"
 
-curl -sS -O http://downloadmirror.intel.com/24910/eng/edison-src-ww18-15.tgz
+curl -s -O http://downloadmirror.intel.com/24910/eng/edison-src-ww18-15.tgz
 
 echo "Unpacking..."
+
+[ -e edison-src ] && rm -rf edison-src
 
 tar zxf edison-src-ww18-15.tgz
 
 cd edison-src
-
 
 echo "Setup..."
 make setup
